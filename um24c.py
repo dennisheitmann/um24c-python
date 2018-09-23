5e7940b86f38ae63d11928cb7cee5a1756a5c779
Dennis Heitmann <dennisheitmann@users.noreply.github.com>
2018-09-23 12:43:10 +0200

    Initial upload

---
+++ um24c.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import serial
+import time
+import datetime
+import sys
+
+rfcomm = serial.Serial(port = "/dev/rfcomm1", baudrate = 9600, timeout = 1)
+rfcomm.flushInput()
+
+sleeptime = 2.5
+
+print("#record steps: "+str(sleeptime)+"s")
+print("#Datetime; Voltage [V]; Current [A]; Power [W]; Temperature [C]")
+
+while True:
+    rfcomm.write(bytes([0xf0]))
+    s = rfcomm.read(130)
+    if (sys.getsizeof(s) < 130): continue
+    voltage = int((ord(chr(s[2])) <<8)| ord(chr(s[3]))) 
+    if (voltage < 2): continue
+    current = int((ord(chr(s[4])) <<8)| ord(chr(s[5])))
+    power = int((ord(chr(s[6])) <<24)| (ord(chr(s[7])) <<16)| (ord(chr(s[8])) <<8)|ord(chr(s[9])))
+    tempC = int((ord(chr(s[10])) <<8)| ord(chr(s[11]))) 
+    print('"'+str(datetime.datetime.now())+'"'+";"+'"'+str(voltage/100.0)+'"'+";"+'"'+str(current/1000.0)+'"'+";"+'"'+str(power/1000.0)+'"'+";"+'"'+str(tempC)+'"'+';')
+    sys.stdout.flush()
+    time.sleep(sleeptime)
+rfcomm.close()
